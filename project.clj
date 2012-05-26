5c89a7b4889493fa7f82e1565ccb1ef2640c6942
Vladimir Epifanov <voldmar@voldmar.ru>
2012-05-27 00:12:22 +0400

    Basic working converter

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.cli "0.2.1"]
+                 [org.clojure/tools.trace "0.7.3"]
                  [hiccup "1.0.0"]
                  [noir "1.3.0-beta3"]]
   :main clj-shower.core
--- src/clj_shower/core.clj
+++ src/clj_shower/core.clj
@@ -1,9 +1,9 @@
 (ns clj-shower.core
-    (:use clj-shower.convert)
     (:use clojure.tools.cli)
-    (:use [clojure.string :only [blank? join]])
+    (:use [clojure.string :only [blank? join trim]])
     (:use hiccup.page)
     (:use hiccup.element))
+
 
 (def ^:dynamic *parse-as-header* false)
 
@@ -26,61 +26,82 @@
 
 (defn h
   "Make header from line, considering that h1 can be only in the first slide"
-  [line]
-  (let [[hashes content] (rest (re-matches #"(#+)\s*(.*)" line))
-        level (min 6 (+ (if (true? *parse-as-header*) 0 1) (count hashes)))]
+  [hashes content]
+  (let [level (min 6 (+ (if (true? *parse-as-header*) 0 1) (count hashes)))]
     (with-meta [(keyword (str "h" level)) content] {:title content})))
 
-(defn id [line]
-  (let [[content] (first (rest (re-matches #"id:\s+(?i)([a-z0-9_-]+)" line)))]
-    (with-meta [] {:id content})))
+(defn id [value]
+  (with-meta [] {:id value}))
 
-(defn cover [line]
-  (let [[src alt w h] (rest (re-matches #"cover:\s+(\S+)\s?(\"[^\"]+\")?(\s+(w))?(\s+(h))?" line))]
-    (with-meta (image src alt) {:class (cons "cover" (remove nil? [w h]))})))
+(defn cover [src alt w h]
+  (with-meta (image (subs src 1 (dec (count src))) alt) {:class (cons "cover" (remove nil? [w h]))}))
 
-(defn shout [line]
-  (let [[content] (first (rest (re-matches #"shout:\s+(?i)(.*)" line)))]
-    (with-meta (when-not (empty? content) [:h2 content]) {:class "shout"}))) 
+(defn shout [content]
+  (with-meta (when-not (empty? content) [:h2 content]) {:class "shout"})) 
 
-(defn p [line]
-  [:p line])
+(defn code [content]
+  (with-meta [:code content] {:wrapper :pre}))
 
+(defn ul [spaces content]
+  (with-meta [:li content] {:wrapper :ul :indent (count spaces)}))
+
+(defn ol [spaces content]
+  (with-meta [:li content] {:wrapper :ol :indent (count spaces)}))
 
 (defn collect [key maps]
   (set (flatten (map key maps))))
 
-(defn parse-line [line]
-  (cond
-    (.startsWith line "#") (h line)
-    (.startsWith line "id:") (id line)
-    (.startsWith line "cover:") (cover line)
-    (.startsWith line "shout:") (shout line)
-    :else (p line)
-    ))
-
-(def RULES 
-     [#"shout:\s+(?i)(.*)"
-      #"cover:\s+(\S+)\s?(\"[^\"]+\")?(\s+(w))?(\s+(h))?"
-      #"id:\s+(?i)([a-z0-9_-]+)"
-      #"(#+)\s*(.*)"
-      #"`(.*)"
-      #"(\s+)[-*]\s*(.*)"
-      #"(\s+)\d\.\s*(.*)"
+(def RULES
+     [#"shout:\s*(?i)(.*)" shout
+      #"cover:\s*(\S+)\s?(\"[^\"]+\")?(\s+w)?(\s+h)?" cover
+      #"id:\s*(?i)([a-z0-9_-]+)" id
+      #"(#+)\s*(.*)" h
+      #"`(.*)" code
+      #"(\s*)[-*]\s*(.*)" ul
+      #"(\s*)\d\.\s*(.*)" ol
       ])
 
-(defn partition-by-type [slide-lines]
-  (lazy-seq 
-    (when-let [s (seq slide-lines)]
-      (let [fst (first s)
+(defn parse-line [line rules]
+  (loop [l line
+         [re f] (take 2 rules)
+         more (drop 2 rules)]
+        (if-not re
+          [:p line]
+          (if-let [match (re-matches re l)]
+            (apply f (next match))
+            (recur l (take 2 more) (drop 2 more))))))
 
-          ]))))
+(defn convert
+  ([parsed-slide-lines]
+   (into []
+         (lazy-seq
+           (when-let [s (seq parsed-slide-lines)]
+                     (let [[fst & xs] s
+                           {wrapper :wrapper indent :indent} (meta fst)]
+                       (if wrapper
+                         [(into [wrapper] (cons fst (convert xs indent)))]
+                         (cons fst (convert xs))))))))
+  ([parsed-slide-lines prev-indent]
+   (lazy-seq
+      (when-let [s (seq parsed-slide-lines)]
+                (let [[fst & other] s
+                      {wrapper :wrapper indent :indent} (meta fst)
+                      grab? #(>= (:indent (meta %)) indent)
+                      grabbed (take-while grab? other)
+                      not-grabbed (drop (count grabbed) other) ]
+                  (if (> indent prev-indent)
+                    (into [[wrapper fst (convert grabbed indent)]]
+                          (convert not-grabbed prev-indent))
+                    (into [fst] (convert grabbed indent))))))))
+
+(defn converter [parsed-slide-lines]
+  (vec (convert parsed-slide-lines)))
 
 (defn parse-slide-lines
   "Convert slide lines to slide (in Hiccup markup)"
   [slide-lines]
-  (let [parsed (map parse-line (partition-by  slide-lines))
-        slide-meta-data (map meta parsed)
+  (let [converted (convert (map #(parse-line % RULES) slide-lines)) 
+        slide-meta-data (map meta converted)
         id (first (map :id slide-meta-data))
         classes (->> (map :class slide-meta-data)
                      (remove nil?)
@@ -92,8 +113,8 @@
         attrs {:class classes}
         attrs (if (nil? id) attrs (assoc attrs :id id))]
     (if (true? *parse-as-header*)
-      [:header.caption (filter not-empty parsed)]
-      [:section attrs (filter not-empty parsed)])))
+      (with-meta (into [:header.caption] (remove empty? converted)) {:title title})
+      (into [:section attrs] (remove empty? converted)))))
 
 (defn translate
   "Translate lines to .html"
@@ -101,13 +122,16 @@
   (let [[header & other] (lazy-slides-seq lines)
         header (binding [*parse-as-header* true] (parse-slide-lines header))
         other (map parse-slide-lines other)]
+    ;(pprint header)
+    ;(pprint other)
     (html5
       [:head
        (include-css "/shower/themes/ribbon/styles/screen.css")
        [:title (:title (meta header))]]
       [:body
        (cons header other)
-       (include-js "/shower/scripts/script.js") ])))
+       (include-js "/shower/scripts/script.js") ])
+    ))
 
 (defn translate-file
   "Translate .shower to .html"
@@ -121,6 +145,9 @@
   (let [[options args banner] (cli args
                                    ["--shower" ".shower source"]
                                    ["--html" ".html output"])
-        {:keys [shower html]} options]
-    (println (translate-file shower))))
+        {:keys [shower html]} options
+        converted (translate-file shower)]
+    ;(println converted)
+    (with-open [wrtr (clojure.java.io/writer html)]
+               (.write wrtr converted))))
 
